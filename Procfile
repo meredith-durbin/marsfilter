527fa54b24456fdc7418c298d86b6953379f7f68
mjd12010 <meredith.durbin@gmail.com>
2016-06-20 16:08:49 +0100

    Attempt to solve failure to bind to port

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: python website/website.py
-heroku ps:scale web=1
--- website/website.py
+++ website/website.py
@@ -46,4 +46,6 @@ def main():
 
 if __name__ == '__main__':
 	app.debug = False
-	app.run()
+	port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+	#app.run()
